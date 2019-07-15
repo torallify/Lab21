f0cb6b04480af438daedbf588cbd44a305a3a086
torallify <torallify@gmail.com>
2019-07-15 18:54:40 -0400

    first commit

---
+++ GCCoffee/Models/Register.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using System.ComponentModel.DataAnnotations;
+
+namespace GCCoffee.Models
+{
+    public class Register
+    {
+		public string UserName { get; set; }
+
+		[EmailAddress]
+		public string Email { get; set; }
+
+        [Password]
+		public string Password { get; set; }
+	}
+}
